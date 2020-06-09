21dfe402201079c7815dee3d58c4b9844a577ed6
Aaron.Berndt <Aaron.Berndt@target.com>
2020-06-09 08:19:51 -0500

    Changed DamageBox to useReducer

--- src/molecules/CharacterSheetComponents/DamageBox.tsx
+++ src/molecules/CharacterSheetComponents/DamageBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import styled from "styled-components";
 import { Button, TextField } from "@material-ui/core";
 
@@ -17,13 +17,24 @@ background:white
 `;
 
 function DamageBox({ healthTotal }: DamageBoxProps) {
-  const [damage, setDamage] = useState(0);
   const [amount, setAmount] = useState(0);
+  const [damage, dispatch] = useReducer(reducer, 0);
+  function reducer(state, action) {
+    const actionObject = {
+      damage: () => state + action.value,
+      heal: () => state - action.value,
+      toZero: () => 0,
+    };
 
-  const addDamage = () => setDamage(damage + Number(amount));
+    return actionObject[action.type]();
+  }
+
+  const addDamage = () => dispatch({ type: "damage", value: Number(amount) });
   const removeDamage = () => {
     const newDamage = damage - Number(amount);
-    setDamage(newDamage <= 0 ? 0 : newDamage);
+    dispatch(
+      newDamage <= 0 ? { type: "toZero" } : { type: "heal", value: amount }
+    );
   };
   const onChange = (e) => setAmount(e.target.value);
 
@@ -32,6 +43,7 @@ function DamageBox({ healthTotal }: DamageBoxProps) {
       <p>{`Damage: ${damage}`}</p>
       {damage === healthTotal ? <p>Make Death Saves</p> : null}
       <TextField onChange={onChange} />
+
       <DamageButton variant="contained" onClick={addDamage}>
         Damage
       </DamageButton>
--- src/molecules/CharacterSheetComponents/HealthBox.tsx
+++ src/molecules/CharacterSheetComponents/HealthBox.tsx
@@ -2,15 +2,12 @@ import React from "react";
 
 interface HealthBoxProps {
   healthTotal: number;
-  damage: number;
 }
 
-function HealthBox({ healthTotal, damage }: HealthBoxProps) {
+function HealthBox({ healthTotal }: HealthBoxProps) {
   return (
     <>
       <p>{`Health: ${healthTotal}`}</p>
-      <p>{`Damage: ${damage}`}</p>
-      {damage === healthTotal ? <p>Make Death Saves</p> : null}
     </>
   );
 }
