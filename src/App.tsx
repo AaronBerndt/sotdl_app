746f7dbcbd82d715cdc202783f1e27ae6ca882cc
Aaron.Berndt <Aaron.Berndt@target.com>
2020-06-17 07:20:33 -0500

    Revert changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-/* import Tabbar from "./components/Tabbar"; */
-/* import TabObject from "./components/TabObject"; */
+import Tabbar from "./components/Tabbar";
+import TabObject from "./components/TabObject";
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
-      {/* <Tabbar tabs={TabObject} /> */}
+      <Tabbar tabs={TabObject} />
     </div>
   );
 }
