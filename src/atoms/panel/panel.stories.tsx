c8f6de3b5aae764ec095110f7d4fd46d838020a1
Aaron.Berndt <Aaron.Berndt@target.com>
2020-06-16 21:40:50 -0500

    Changed Panel Import

--- src/atoms/panel/panel.stories.tsx
+++ src/atoms/panel/panel.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import Panel from "./index";
+import Panel from "./Panel";
 
 export default {
   title: "Atoms/Panel",
