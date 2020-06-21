28c816b178eb72f86cfe38bcc447da6c5e695be4
Aaron.Berndt <Aaron.Berndt@target.com>
2020-06-21 16:14:44 -0500

    Added color to textfield

--- src/organisms/DiceRoller/BBBox.tsx
+++ src/organisms/DiceRoller/BBBox.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
-import { TextField as MaterialTextField, Button } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
 import { isZero } from "../../utilities";
 import DiceRollContext from "../../context/DiceRollContext";
 
@@ -10,10 +10,6 @@ const BaneButton = styled(Button)`
 
 const BoonButton = styled(Button)`
   background-color: lightBlue;
-`;
-
-const BBTextField = styled(MaterialTextField)`
-  color: "red";
 `;
 
 function BBBox() {
@@ -30,15 +26,24 @@ function BBBox() {
     ? boonAmount
     : baneAmount;
 
+  const color = isZero(boonAmount - baneAmount)
+    ? "black"
+    : boonAmount > baneAmount
+    ? "lightBlue"
+    : "red";
   return (
     <>
-      <BBTextField
+      <TextField
         label="Boon/Banes"
         variant="outlined"
         defaultValue={0}
         value={defaultAmount}
         size="small"
-        disabled
+        InputProps={{
+          style: {
+            color: color,
+          },
+        }}
       />
 
       <BoonButton onClick={() => addBoons()}>Add Boon</BoonButton>
