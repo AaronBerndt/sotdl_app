42e096190cb62bfdd0010c6b91eace26d0f5faf2
Aaron.Berndt <Aaron.Berndt@target.com>
2020-07-01 21:07:01 -0500

    Readded files

---
+++ src/atoms/Dialog/dialog.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Dialog from "./Dialog";
+import { action } from "@storybook/addon-actions/dist/preview";
+
+export default {
+  title: "Atoms/Dialog",
+  component: Dialog,
+};
+
+const mockStatus = (openStatus: boolean) => ({ open: openStatus });
+
+export const isOpen = () => (
+  <Dialog
+    title="Confirm Ancestry"
+    actionName="Confirm Ancestry"
+    isOpen={mockStatus(true)}
+    onClickFuncion={action("button-click")}
+  />
+);
+
+export const notOpen = () => (
+  <Dialog
+    title="Confirm Ancestry"
+    actionName="Confirm Ancestry"
+    isOpen={mockStatus(false)}
+    onClickFuncion={action("button-click")}
+  />
+);
